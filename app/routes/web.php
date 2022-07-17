015b5f75d73a9b402308e2ded95e6a9240cbcdc3
tumarinson <tumarinsond@gmail.com>
2022-07-17 13:34:05 +0300

    added default route

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,7 +2,9 @@
 
 $webRoutes = [
     /** @see \app\Controller\UsersController::index() */
+    '' => 'app/Controller/UsersController@index',
     'users' => 'app/Controller/UsersController@index',
+
     /** @see \app\Controller\UsersController::view() */
     'users/(\d+)/view' => 'app/Controller/UsersController@view',
 ];
