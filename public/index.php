93b1f3db748f0da3c60cbe71649eedb779ba1f7f
tumarinson <tumarinsond@gmail.com>
2022-07-27 22:26:55 +0300

    autoloading classes

--- public/index.php
+++ public/index.php
@@ -2,13 +2,14 @@
 
 use bootstrap\Router;
 
-define("APP_ROOT_DIRECTORY", initRootDirectory());
+ini_set('display_errors', 1);
 
-include "../bootstrap/Router.php";
+define("APP_ROOT_DIRECTORY", initRootDirectory());
 
 bootApp();
 
 function bootApp() {
+    initAutoloadingClasses();
     $requestUri = $_SERVER['REQUEST_URI'];
     $router = new Router();
     $router->route($requestUri);
@@ -16,4 +17,11 @@ function bootApp() {
 
 function initRootDirectory() {
     return realpath($_SERVER['DOCUMENT_ROOT'] . '/../') . '/';
+}
+
+function initAutoloadingClasses() {
+    spl_autoload_register(function ($className) {
+        $className = str_replace('\\', '/', $className);
+        include_once APP_ROOT_DIRECTORY . $className . '.php';
+    });
 }
